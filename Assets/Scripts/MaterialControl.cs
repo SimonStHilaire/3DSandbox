d89f79b0bfb4f33ac668bf1ef0dfec0aebfbbf91
SimonStHilaire <simon.sthilaire@gmail.com>
2021-04-26 16:03:42 -0400

    Add water and glow shaders

---
+++ Assets/Scripts/MaterialControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class MaterialControl : MonoBehaviour
+{
+    public float Factor;
+    Material TargetMaterial;
+    public float Speed;
+
+    public enum GlowType
+    {
+        Show,
+        Hide
+    }
+
+    public GlowType glowType;
+
+    void Start()
+    {
+        TargetMaterial = GetComponent<Renderer>().sharedMaterial;
+    }
+
+    void Update()
+    {
+        switch(glowType)
+        {
+            case GlowType.Hide:
+                Factor -= Time.deltaTime * Speed;
+                Factor = Mathf.Clamp01(Factor);
+                break;
+            case GlowType.Show:
+                Factor += Time.deltaTime * Speed;
+                Factor = Mathf.Clamp01(Factor);
+                break;
+        }
+
+        TargetMaterial.SetFloat("_factor", Factor);
+    }
+}
