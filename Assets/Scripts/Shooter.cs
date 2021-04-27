46502427fe3bbd0b2992f2fd4b0bc5c42bfdafcb
SimonStHilaire <simon.sthilaire@gmail.com>
2021-04-26 23:22:16 -0400

    Rendertexture + trajectory

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public Transform Target;
+
+    public GameObject ProjectilePrefab;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+
+        }
+    }
+}
