9de3126ff0dec4249014452397e0cc8a1a98965e
SimonStHilaire <simon.sthilaire@gmail.com>
2021-05-05 13:09:46 -0400

    Evaluation 1

---
+++ Assets/Scripts/AnimController.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimController : MonoBehaviour
+{
+    Animator Controller;
+    public float Speed;
+    public float StepSize;
+
+    void Start()
+    {
+        Controller = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * Speed * StepSize;
+        Controller.SetFloat("Speed", Speed);
+    }
+}
---
+++ Assets/Scripts/EvaluationManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EvaluationManager : MonoBehaviour
+{
+    public Material ControlledMaterial;
+    public Slider MaterialSlider;
+
+    public Transform Earth;
+    public Transform Moon;
+    public Transform Mars;
+
+    public float EarthSpeed;
+
+    public Text MoonMarsDistanceText;
+    public Text MoonCamrFAngleText;
+
+    public Transform Rock;
+    public float RockSpeed;
+
+    public void OnSliderChanged()
+    {
+        //TODO:
+        //ControlledMaterial
+    }
+
+    public void Update()
+    {
+        Earth.Rotate(new Vector3(0f, Time.deltaTime * EarthSpeed, 0f));
+        
+        //TODO: Distance Moon-mars
+        float distance = 0f;
+        MoonMarsDistanceText.text = distance.ToString();
+
+        //TODO: Angle: Moon / Camera Forward
+        float angle = 0f;
+        MoonCamrFAngleText.text = angle.ToString();
+
+        //TODO: Update Rock
+        //Rock
+
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        //TODO: Afficher la normale créée par le vecteur up (0,1,0) et le vecteur entre "Moon" et "Earth"
+        //Gizmos.DrawLine(from, to, Color.red);
+    }
+#endif //UNITY_EDITOR
+}
