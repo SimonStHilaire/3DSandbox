ac93eb3c96d394aeeb15607c752d386fadd393c0
SimonStHilaire <simon.sthilaire@gmail.com>
2021-05-11 20:11:58 -0400

    SkeletonRigging scene wip

---
+++ Assets/Scripts/HitTarget.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitTarget : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<Renderer>().enabled = false;
+        Invoke("Reactivate", 2f);
+    }
+
+    void Reactivate()
+    {
+        GetComponent<Renderer>().enabled = true;
+    }
+}
---
+++ Assets/Scripts/SkeletonController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class SkeletonController : MonoBehaviour
+{
+    public float ForwardSpeed;
+    public float TurnSpeed;
+    public Animator AnimController;
+
+    CharacterController Controller;
+    
+
+    void Start()
+    {
+        Controller = GetComponent<CharacterController>();    
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") > 0f)
+        {
+            Vector3 move = transform.forward * ForwardSpeed * Time.deltaTime;
+
+            Controller.Move(move);
+
+            AnimController.SetBool("Walk", true);
+        }
+        else
+        {
+            AnimController.SetBool("Walk", false);
+        }
+
+        Controller.transform.Rotate(new Vector3(0f, Input.GetAxis("Horizontal") * TurnSpeed * Time.deltaTime, 0f));
+
+        //Attack
+    }
+}
