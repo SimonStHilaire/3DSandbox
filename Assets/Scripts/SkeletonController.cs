d1e150a9b99690d77e09cb7751e998b9000fd5b5
SimonStHilaire <simon.sthilaire@gmail.com>
2021-05-12 14:25:49 -0400

    Add Cinemachine

--- Assets/Scripts/SkeletonController.cs
+++ Assets/Scripts/SkeletonController.cs
@@ -2,24 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
 public class SkeletonController : MonoBehaviour
 {
     public float ForwardSpeed;
     public float TurnSpeed;
     public Animator AnimController;
+    public Collider SwordCollider;
+    public CharacterController Controller;
 
-    CharacterController Controller;
-    
+    private bool CanMove = true;
 
     void Start()
     {
-        Controller = GetComponent<CharacterController>();    
+ 
     }
 
     void Update()
     {
-        if (Input.GetAxis("Vertical") > 0f)
+        if (CanMove && Input.GetAxis("Vertical") > 0f)
         {
             Vector3 move = transform.forward * ForwardSpeed * Time.deltaTime;
 
@@ -32,8 +32,33 @@ public class SkeletonController : MonoBehaviour
             AnimController.SetBool("Walk", false);
         }
 
-        Controller.transform.Rotate(new Vector3(0f, Input.GetAxis("Horizontal") * TurnSpeed * Time.deltaTime, 0f));
+        if(CanMove)
+            Controller.transform.Rotate(new Vector3(0f, Input.GetAxis("Horizontal") * TurnSpeed * Time.deltaTime, 0f));
 
         //Attack
+        if (CanMove && Input.GetButton("Fire1"))
+        {
+            AnimController.SetTrigger("Attack");
+        }
+    }
+
+    public void ActivateMove()
+    {
+        CanMove = true;
+    }
+
+    public void DeactivateMove()
+    {
+        CanMove = false;
+    }
+
+    public void ActivateSword()
+    {
+        SwordCollider.enabled = true;
+    }
+
+    public void DeactivateSword()
+    {
+        SwordCollider.enabled = false;
     }
 }
