c5e95535a3c54423f3286f08abd72c45352135fe
SimonStHilaire <simon.sthilaire@gmail.com>
2021-06-07 15:13:17 -0400

    AttachmentPoints

---
+++ Assets/Scripts/Customization/AttachmentPoint.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachmentPoint : MonoBehaviour
+{
+    public AttachmentType type;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
