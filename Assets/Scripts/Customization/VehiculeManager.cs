4dc397cd19819234e8fb31c114e8e3332debe3ac
SimonStHilaire <simon.sthilaire@gmail.com>
2021-06-16 14:41:37 -0400

    SetAttachment

--- Assets/Scripts/Customization/VehiculeManager.cs
+++ Assets/Scripts/Customization/VehiculeManager.cs
@@ -13,18 +13,78 @@ public class VehiculeManager : MonoBehaviour
     public AttachmentPoint RoofAttachmentPoint;
     public AttachmentPoint BulbarAttachmentPoint;
     public AttachmentPoint HeadlightAttachmentPoint;
-    public AttachmentPoint TireFrontRightAttachmentPoint;
-    public AttachmentPoint TireFrontLeftAttachmentPoint;
-    public AttachmentPoint TireBackRightAttachmentPoint;
-    public AttachmentPoint TireBackLeftAttachmentPoint;
+    public List<AttachmentPoint> TireAttachmentPoints;//FR, FL, BR, BL
 
-    void Start()
-    {
-        
-    }
+    GameObject RoofPart = null;
+    GameObject BulbarPart = null;
+    GameObject HeadlightPart = null;
+    GameObject[] TireParts = null;
 
-    void Update()
+    public void SetAttachment(AttachmentPart part)
     {
-        
+        switch(part.type)
+        {
+            case AttachmentType.Roof:
+                {
+                    if(RoofPart != null)
+                    {
+                        Destroy(RoofPart.gameObject);
+                    }
+
+                    RoofPart = Instantiate(part.gameObject);
+
+                    RoofPart.transform.SetParent(RoofAttachmentPoint.transform, false);
+                    RoofPart.gameObject.SetActive(true);
+
+                    break;
+                }
+            case AttachmentType.Bulbar:
+                {
+                    if (BulbarPart != null)
+                    {
+                        Destroy(BulbarPart.gameObject);
+                    }
+
+                    BulbarPart = Instantiate(part.gameObject);
+
+                    BulbarPart.transform.SetParent(BulbarAttachmentPoint.transform, false);
+                    BulbarPart.gameObject.SetActive(true);
+                    break;
+                }
+            case AttachmentType.Headlight:
+                {
+                    if (HeadlightPart != null)
+                    {
+                        Destroy(HeadlightPart.gameObject);
+                    }
+
+                    HeadlightPart = Instantiate(part.gameObject);
+
+                    HeadlightPart.transform.SetParent(HeadlightAttachmentPoint.transform, false);
+                    HeadlightPart.gameObject.SetActive(true);
+                    break;
+                }
+            case AttachmentType.Tire:
+                {
+                    if(TireParts != null)
+                    {
+                        foreach (GameObject tire in TireParts)
+                            Destroy(tire);
+                    }
+                    else
+                    {
+                        TireParts = new GameObject[4];
+                    }
+
+                    for (int i = 0; i < 4; ++i)
+                    {
+                        TireParts[i] = Instantiate(part.gameObject);
+                        TireParts[i].transform.SetParent(TireAttachmentPoints[i].transform, false);
+                        TireParts[i].SetActive(true);
+                    }
+
+                    break;
+                }
+        }
     }
 }
